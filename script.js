3376d4b94c8edbad1a1325f631ee4edaa453cb0f
Vipul Aggarwal <vipul.captureart@gmail.com>
2020-03-22 16:03:48 +0530

    Create script.js
    Completed Javascript First Task.

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Added Variable for myName and myCourses  */
+let myName = "Vipul Aggarwal";
+let myCourses = ["HTML", "CSS", "JavaScript", "Node JS"];
+
+/* Print  myName and myCourses using the variables */
+console.log(myName);
+console.log(myCourses);
+
+/* Write the function using for loop and used an if statement to find the values in even */
+function evenNumbers() {
+    for(let i = 1; i <= 200; i++) {
+        if(i%2 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+/* Calling the Function */
+evenNumbers();
